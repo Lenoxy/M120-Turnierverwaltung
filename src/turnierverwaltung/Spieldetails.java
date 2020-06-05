cd71d7662b3b543b70426462bfbedb5dacc15e07
Leo Scherer <lenoxy@gmx.ch>
2020-06-05 18:48:47 +0200

    GUI added

---
+++ src/turnierverwaltung/Spieldetails.java
@@ -0,0 +1,22 @@
+package turnierverwaltung;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Spieldetails extends Application{
+    @Override
+    public void start(Stage stage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("spieldetails.fxml"));
+        stage.setTitle("Spieldetails");
+        stage.setResizable(false);
+        stage.setScene(new Scene(root, 300, 175));
+        stage.show();
+    }
+
+    public static void main(String[] args){
+        launch(args);
+    }
+}
