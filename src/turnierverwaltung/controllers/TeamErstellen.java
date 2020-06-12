1b199dd4d21c528c3f3e21586de825cd0cd86779
the-philip-baumann <philip.baumann@hispeed.ch>
2020-06-12 11:08:26 +0200

    Fix paths to fxmls

--- src/turnierverwaltung/controllers/TeamErstellen.java
+++ src/turnierverwaltung/controllers/TeamErstellen.java
@@ -9,7 +9,7 @@ import javafx.stage.Stage;
 public class TeamErstellen extends Application{
     @Override
     public void start(Stage stage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("./../teamErstellen.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("./../views/teamErstellen.fxml"));
         stage.setTitle("Team Erstellen");
         stage.setResizable(false);
         stage.setScene(new Scene(root, 300, 175));
