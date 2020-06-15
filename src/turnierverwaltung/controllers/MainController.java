b959e26a9e9ef8eda3d5f7ab8472830fcf7bb063
Leo Scherer <lenoxy@gmx.ch>
2020-06-15 18:48:37 +0200

    Merge conflict solved

--- src/turnierverwaltung/controllers/MainController.java
+++ src/turnierverwaltung/controllers/MainController.java
@@ -4,31 +4,12 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
-<<<<<<< HEAD
-import turnierverwaltung.models.Turnier;
-=======
-
->>>>>>> 5058af493adbe3838d19e1b21c8d7eab8d0c1473
 
 public class MainController{
 
 
 
     public Button teamHinzufuegen;
-
-<<<<<<< HEAD
-    public TableView<turnierverwaltung.models.Team> tabelleTableView;
-
-    public Turnier turnier = new Turnier();
-
-    @FXML
-    public void initialize() {
-        tabelleTableView.setItems(turnier.getTeams());
-=======
-    public MainController () {
-
->>>>>>> 5058af493adbe3838d19e1b21c8d7eab8d0c1473
-    }
 
     public void onTeamHinzufuegen(ActionEvent e){
 
