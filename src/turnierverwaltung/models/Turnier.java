19ce6038cbc16a9ed8276a6aee0b4b393231cee0
Leo Scherer <lenoxy@gmx.ch>
2020-06-25 19:04:07 +0200

    Done - Group allocation

--- src/turnierverwaltung/models/Turnier.java
+++ src/turnierverwaltung/models/Turnier.java
@@ -16,11 +16,11 @@ public class Turnier{
     private int pointsPerDraw = 1;
     private int pointsPerLoss = 0;
 
-    public ObservableList<Group> getGroups() {
+    public ObservableList<Group> getGroups(){
         return groups;
     }
 
-    public void setGroups(ObservableList<Group> groups) {
+    public void setGroups(ObservableList<Group> groups){
         this.groups = groups;
     }
 
@@ -28,13 +28,14 @@ public class Turnier{
 
     public Turnier(){
         System.out.println("Turnier erstellt");
-        teams.add(new Team("test1", 1, "a", 0, 1, 1,  "22:55", "Bucher"));
-        teams.add(new Team("test2", 2, "a", 0, 1, 1,  "22:55", "Bucher"));
-        teams.add(new Team("test3", 4, "a", 0, 1, 1,  "22:55", "Bucher"));
-        teams.add(new Team("test4", 3, "a", 0, 1, 1,  "22:55", "Bucher"));
-        teams.add(new Team("test5", 8, "a", 0, 1, 1,  "22:55", "Bucher"));
-        teams.add(new Team("test6", 6, "a", 0, 1, 1,  "22:55", "Bucher"));
-        teams.add(new Team("test7", 4, "a", 0, 1, 1,  "22:55", "Bucher"));
+        teams.add(new Team("1", 1, "a", 0, 1, 1, "22:55", "Bucher"));
+        teams.add(new Team("2", 2, "a", 0, 1, 1, "22:55", "Bucher"));
+        teams.add(new Team("3", 4, "a", 0, 1, 1, "22:55", "Bucher"));
+        teams.add(new Team("4", 3, "a", 0, 1, 1, "22:55", "Bucher"));
+        teams.add(new Team("5", 8, "a", 0, 1, 1, "22:55", "Bucher"));
+        teams.add(new Team("6", 6, "a", 0, 1, 1, "22:55", "Bucher"));
+        teams.add(new Team("7", 4, "a", 0, 1, 1, "22:55", "Bucher"));
+        teams.add(new Team("8", 4, "a", 0, 1, 1, "22:55", "Bucher"));
     }
 
     public static void restartTurnier(){
@@ -44,31 +45,36 @@ public class Turnier{
     private void reshuffleGroups(){
         final char[] groupNames = new char[]{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K'};
         double teamAmount = this.teams.size();
-        double groupAmmount = Math.ceil(teamAmount/5);
+        double groupAmmount = Math.ceil(teamAmount / 5);
         System.out.println("Teams: " + teamAmount);
         System.out.println("Groups: " + groupAmmount);
-        double teamsPerGroup = Math.floor(teamAmount/groupAmmount);
+        double teamsPerGroup = Math.floor(teamAmount / groupAmmount);
         System.out.println("Teams Per Group: " + teamsPerGroup);
 
-        double teamsModulo = teamAmount%groupAmmount;
+        double teamsModulo = teamAmount % groupAmmount;
         System.out.println("Mod: " + teamsModulo);
 
         double overflowTeams = teamsModulo;
         int teamCounter = 0;
-        for(int groupCounter = 0; groupCounter < groupAmmount; groupCounter++){
-            while(teamCounter < (groupCounter + 1) * teamsPerGroup){
-                setTeamGroup(this.teams.get(teamCounter), String.valueOf(groupNames[groupCounter]));
-                System.out.println(this.teams.get(teamCounter).getTeamName() + "  :  " + groupNames[groupCounter]);
+        for(int groupCounter = 1; groupCounter <= groupAmmount; groupCounter++){
 
+            while(teamCounter < (groupCounter * teamsPerGroup) + overflowTeams){
+                setTeamGroup(this.teams.get(teamCounter), String.valueOf(groupNames[groupCounter-1]));
                 teamCounter++;
             }
+            if(groupCounter==0){
+                overflowTeams=0;
+            }
         }
+        System.out.println("DEBUG: teamCounter: " + teamCounter);
 
 
     }
 
     private void setTeamGroup(Team team, String group){
         team.setGroup(group);
+        System.out.println(team.getTeamName() + "  :  " + group);
+
     }
 
     public static Turnier getInstance(){
