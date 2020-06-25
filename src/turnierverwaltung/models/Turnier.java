a48b2eaa466becfc992e0da7a5507f8d63de5c4e
Leo Scherer <lenoxy@gmx.ch>
2020-06-25 18:10:04 +0200

    WIP Autoadd Group

--- src/turnierverwaltung/models/Turnier.java
+++ src/turnierverwaltung/models/Turnier.java
@@ -3,6 +3,9 @@ package turnierverwaltung.models;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Turnier{
     private static Turnier instance;
     private ObservableList<Team> teams = FXCollections.observableArrayList();
@@ -13,11 +16,41 @@ public class Turnier{
 
     public Turnier(){
         System.out.println("Turnier erstellt");
-        teams.add(new Team("test", 1, "a", 0, 1, 1,  "22:55", "Bucher"));
+        teams.add(new Team("test", 1, "a", 0, 1, 1, "22:55", "Bucher"));
     }
 
     public static void restartTurnier(){
         instance = new Turnier();
+    }
+
+    private void reshuffleGroups(){
+        final char[] groupNames = new char[]{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K'};
+        double teamAmount = this.teams.size();
+        double groupAmmount = Math.ceil(teamAmount/5);
+        System.out.println("Teams: " + teamAmount);
+        System.out.println("Groups: " + groupAmmount);
+        double teamsPerGroup = Math.floor(teamAmount/groupAmmount);
+        System.out.println("Teams Per Group: " + teamsPerGroup);
+
+        double teamsModulo = teamAmount%groupAmmount;
+        System.out.println("Mod: " + teamsModulo);
+
+        double overflowTeams = teamsModulo;
+        int teamCounter = 0;
+        for(int groupCounter = 0; groupCounter < groupAmmount; groupCounter++){
+            while(teamCounter < (groupCounter + 1) * teamsPerGroup){
+                setTeamGroup(this.teams.get(teamCounter), String.valueOf(groupNames[groupCounter]));
+                System.out.println(this.teams.get(teamCounter).getTeamName() + "  :  " + groupNames[groupCounter]);
+
+                teamCounter++;
+            }
+        }
+
+
+    }
+
+    private void setTeamGroup(Team team, String group){
+        team.setGroup(group);
     }
 
     public static Turnier getInstance(){
@@ -35,17 +68,19 @@ public class Turnier{
 
     public void addTeam(Team newTeam){
         this.teams.add(newTeam);
+        this.reshuffleGroups();
     }
 
     public void removeTeam(Team team){
         this.teams.remove(team);
+        this.reshuffleGroups();
     }
 
     public void removeSpiel(Spiel spiel){
         this.spiele.remove(spiel);
     }
 
-    public void setSpiel (Spiel spiel){
+    public void setSpiel(Spiel spiel){
         this.spiele.add(spiel);
     }
 
