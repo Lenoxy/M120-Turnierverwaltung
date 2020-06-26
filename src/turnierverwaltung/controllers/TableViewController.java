7a24d10fd6adefafd8aae0d74d319fc5db9c048a
the-philip-baumann <philip.baumann@hispeed.ch>
2020-06-26 11:13:28 +0200

    TableView clean up

--- src/turnierverwaltung/controllers/TableViewController.java
+++ src/turnierverwaltung/controllers/TableViewController.java
@@ -19,9 +19,11 @@ import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
-public class TableViewController implements Initializable{
+public class TableViewController implements Initializable {
 
 
     //Tabelle
@@ -48,10 +50,9 @@ public class TableViewController implements Initializable{
     @FXML
     TextField pointsPerVictory, pointsPerDraw, pointsPerLoss;
 
+    List<TextField> pointsSettings = new ArrayList<>();
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources){
-        // Tabelle
+    private void setupTableTabelle() {
         teamNameColumn.setCellValueFactory(new PropertyValueFactory<Team, String>("teamName"));
         pointsColumn.setCellValueFactory(new PropertyValueFactory<Team, String>("points"));
         groupColumn.setCellValueFactory(new PropertyValueFactory<Team, String>("group"));
@@ -59,69 +60,80 @@ public class TableViewController implements Initializable{
         lossesColumn.setCellValueFactory(new PropertyValueFactory<Team, String>("losses"));
         drawsColumn.setCellValueFactory(new PropertyValueFactory<Team, String>("draws"));
         goalDifferentialColumn.setCellValueFactory(new PropertyValueFactory<Team, String>("goalDifferential"));
-
         tableViewTabelle.setItems(Turnier.getInstance().getTeams());
+    }
 
-        System.out.println(team1);
-        System.out.println(teamNameColumn);
-
-
-
-        // Spielplan
+    private void setupTableSpielplan() {
         team1.setCellValueFactory(new PropertyValueFactory<Spiel, String>("team1"));
         team2.setCellValueFactory(new PropertyValueFactory<Spiel, String>("team2"));
         result.setCellValueFactory(new PropertyValueFactory<Spiel, String>("resultat"));
         time.setCellValueFactory(new PropertyValueFactory<Spiel, String>("time"));
         spielplanTableView.setItems(Turnier.getInstance().getSpiele());
+    }
 
-        // Teams verwalten
+    private void setupTableTeamVerwalten() {
         teamTeamNameColumn.setCellValueFactory(new PropertyValueFactory<Team, String>("teamName"));
         teamPointsColumn.setCellValueFactory(new PropertyValueFactory<Team, String>("points"));
         teamGroupColumn.setCellValueFactory(new PropertyValueFactory<Team, String>("group"));
-
         teamsTableView.setItems(Turnier.getInstance().getTeams());
+    }
 
-
+    private void setupSettingsTab() {
         pointsPerVictory.setText(String.valueOf(Turnier.getInstance().getPointsPerVictory()));
         pointsPerDraw.setText(String.valueOf(Turnier.getInstance().getPointsPerDraw()));
         pointsPerLoss.setText(String.valueOf(Turnier.getInstance().getPointsPerLoss()));
+    }
 
-
+    private void settingsTabConstraints() {
         pointsPerVictory.textProperty().addListener((observable, oldValue, newValue) -> {
-            if(! newValue.matches("\\d*") || newValue.isEmpty()){
+            if (!newValue.matches("\\d*") || newValue.isEmpty()) {
                 pointsPerVictory.setText(newValue.replaceAll("[^\\d]", ""));
-            }else{
+            } else {
                 Turnier.getInstance().setPointsPerVictory(Integer.parseInt(newValue));
                 System.out.println("Set pointsPerWin to " + newValue);
             }
         });
+
         pointsPerDraw.textProperty().addListener((observable, oldValue, newValue) -> {
-            if(! newValue.matches("\\d*") || newValue.isEmpty()){
+            if (!newValue.matches("\\d*") || newValue.isEmpty()) {
                 pointsPerDraw.setText(newValue.replaceAll("[^\\d]", ""));
-            }else{
+            } else {
                 Turnier.getInstance().setPointsPerDraw(Integer.parseInt(newValue));
                 System.out.println("Set pointsPerDraw to " + newValue);
             }
         });
         pointsPerLoss.textProperty().addListener((observable, oldValue, newValue) -> {
-            if(! newValue.matches("\\d*") || newValue.isEmpty()){
+            if (!newValue.matches("\\d*") || newValue.isEmpty()) {
                 pointsPerLoss.setText(newValue.replaceAll("[^\\d]", ""));
-            }else{
+            } else {
                 Turnier.getInstance().setPointsPerLoss(Integer.parseInt(newValue));
                 System.out.println("Set pointsPerLoss to " + newValue);
             }
         });
     }
 
-    public void onTurnierNeustarten(){
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        setupTableTabelle();
+        setupTableSpielplan();
+        setupTableTeamVerwalten();
+        setupSettingsTab();
+        pointsSettings.add(pointsPerDraw);
+        pointsSettings.add(pointsPerLoss);
+        pointsSettings.add(pointsPerVictory);
+        settingsTabConstraints();
+    }
+
+    public void onTurnierNeustarten() {
         Turnier.restartTurnier();
     }
 
-    public void onRemoveSelectedTeam(Event e){
+    public void onRemoveSelectedTeam(Event e) {
         Team selected = teamsTableView.getSelectionModel().getSelectedItem();
-        if(selected != null){
+        if (selected != null) {
             Turnier.getInstance().removeTeam(selected);
-        }else{
+        } else {
             // Error Sound effect
             final Runnable runnable = (Runnable) Toolkit.getDefaultToolkit().getDesktopProperty("win.sound.default");
             if (runnable != null) {
@@ -131,15 +143,15 @@ public class TableViewController implements Initializable{
 
     }
 
-    public void onCreateTeam(Event e) throws IOException{
-        try{
+    public void onCreateTeam(Event e) throws IOException {
+        try {
             Parent root = FXMLLoader.load(getClass().getResource("../views/teamErstellen.fxml"));
             Stage stage = new Stage();
             stage.setTitle("Team Erstellen");
             stage.setResizable(false);
             stage.setScene(new Scene(root, 300, 175));
             stage.show();
-        }catch(Exception ex){
+        } catch (Exception ex) {
             System.out.println(ex);
         }
     }
